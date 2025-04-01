ee015fc6c6e869d06560a44c2b88fd2fe842a5bb
auplisas <aupli16@freeuni.edu.ge>
2025-04-01 21:09:01 +0200

    add: texture palcement functions moved into separate file

--- src/raycasting/texture_placement.c
+++ src/raycasting/texture_placement.c
@@ -6,7 +6,7 @@
 /*   By: auplisas <auplisas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 18:55:44 by auplisas          #+#    #+#             */
-/*   Updated: 2025/04/01 19:32:06 by auplisas         ###   ########.fr       */
+/*   Updated: 2025/04/01 21:08:13 by auplisas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,14 +44,10 @@ void	calculate_ray_x_hit(t_game *game, t_dda *dda)
 		- floor(dda->hit_result->exact_hit_x);
 }
 
-void	draw_wall_slice(t_game *game, int ray_x, t_dda *dda)
+mlx_texture_t	*select_texture(t_game *game, t_dda *dda)
 {
 	mlx_texture_t	*tex;
-	float			step;
-	uint32_t		color;
-	int				y;
 
-	y = dda->draw->draw_start;
 	if (dda->hit_result->side == 0)
 	{
 		if (dda->ray->ray_dir_x > 0)
@@ -66,21 +62,40 @@ void	draw_wall_slice(t_game *game, int ray_x, t_dda *dda)
 		else
 			tex = game->texture_data->south_texture;
 	}
-	step = 1.0 * tex->height / dda->draw->line_height;
-	dda->draw->texture_vertical_pos = (y - game->config->s_height / 2
-			+ dda->draw->line_height / 2) * step;
-	calculate_ray_x_hit(game, dda);
-	dda->draw->texture_horizontal_pos = (int)(dda->hit_result->exact_hit_x
-			* tex->width);
+	return (tex);
+}
+
+void	mirror_effect_correction(t_dda *dda,
+		mlx_texture_t *texture)
+{
 	if ((dda->hit_result->side == 0 && dda->ray->ray_dir_x < 0)
 		|| (dda->hit_result->side == 1 && dda->ray->ray_dir_y > 0))
-		dda->draw->texture_horizontal_pos = tex->width
+		dda->draw->texture_horizontal_pos = texture->width
 			- dda->draw->texture_horizontal_pos - 1;
+}
+
+void	draw_wall_slice(t_game *game, int ray_x, t_dda *dda)
+{
+	mlx_texture_t	*texture;
+	float			step;
+	uint32_t		color;
+	int				y;
+
+	y = dda->draw->draw_start;
+	texture = select_texture(game, dda);
+	step = 1.0 * texture->height / dda->draw->line_height;
+	calculate_ray_x_hit(game, dda);
+	dda->draw->texture_vertical_pos = (y - game->config->s_height / 2
+			+ dda->draw->line_height / 2) * step;
+	dda->draw->texture_horizontal_pos = (int)(dda->hit_result->exact_hit_x
+			* texture->width);
+	mirror_effect_correction(dda, texture);
 	while (y < dda->draw->draw_end)
 	{
-		dda->draw->tex_y = (int)dda->draw->texture_vertical_pos % tex->height;
-		color = fix_color(((uint32_t *)tex->pixels)[dda->draw->tex_y
-				* tex->width + dda->draw->texture_horizontal_pos]);
+		dda->draw->tex_y = (int)dda->draw->texture_vertical_pos
+			% texture->height;
+		color = fix_color(((uint32_t *)texture->pixels)[dda->draw->tex_y
+				* texture->width + dda->draw->texture_horizontal_pos]);
 		mlx_put_pixel(game->player_data->player, ray_x, y, color);
 		dda->draw->texture_vertical_pos += step;
 		y++;
