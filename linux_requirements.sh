a96ae2ef0a85785e96727f786234afe9aea9ea4b
AbdallahZerfaoui <82400944+AbdallahZerfaoui@users.noreply.github.com>
2025-04-02 23:44:40 +0200

    Update linux_requirements.sh

--- linux_requirements.sh
+++ linux_requirements.sh
@@ -3,4 +3,5 @@
 # Install the required packages
 echo "Installing required packages for Linux"
 apt-get update
+apt-get install -y make git g++ libncurses5-dev libncursesw5-dev
 apt-get install -y cmake libgl1-mesa-dev libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libasound2-dev libglfw3-dev
